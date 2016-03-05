4934161cbcc71876436b581de1cb6da50961ef21
victorsilent <joaovpbarbosa@gmail.com>
2016-03-04 22:34:11 -0300

    Tests and validations :)

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,14 @@
 class Product < ActiveRecord::Base
+
+	validates :title, :description, :image_url, presence: true
+
+	#Numericalmente maior ou igual a 0.01, isso pq levamos em conta apenas 2 casa decimais no banco
+	validates :price, numericality: {greater_than_or_equal_to: 0.01} 
+
+	validates :title, uniqueness: true
+
+	validates :image_url, allow_blank: true, format: {
+		with: %r{\.(gif|jpg|png\Z)}i,
+		message: 'must be a URL for GIF, JPG or PNG image'
+	}
 end
